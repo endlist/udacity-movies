340223bfe9760adaed242e49314fca8fb803298f
Li Wren <self@endlist.io>
2017-03-13 15:26:22 -0600

    add basic tests for all movie functions

--- src/media.py
+++ src/media.py
@@ -8,12 +8,12 @@ except ImportError:
 import json
 import tmdb_config
 
-# you will need to create a tmdb_config file with your own API key
-API_KEY = tmdb_config.get_key()
-BASE_API_URL = 'https://api.themoviedb.org/3/'
-
 class Movie():
     """This class provides a way to store movie-related information."""
+
+    # you will need to create a tmdb_config file with your own API key
+    API_KEY = tmdb_config.get_key()
+    BASE_API_URL = 'https://api.themoviedb.org/3/'
 
     def __init__(self, options):
         if not options.has_key('title'):
@@ -26,35 +26,49 @@ class Movie():
         self.data['id'] = self.get_movie_id(options)
 
         # get trailer from API
-        self.data['trailer_id'] = self.get_trailer_url(self.data['id'])
+        self.data['trailer_id'] = self.get_trailer_url(self.data)
 
         # get other movie metadata from API
-        self.data.update(self.get_movie_data(self.data['id']))
+        self.data.update(self.get_movie_data(self.data))
 
-    def get_movie_id(self, options):
-        # if there is a year specification, use it,
+    def get_year_query(self, options):
         year_query = ''
         if (('year' in options) and options['year']):
             year_query = '&year=' + options['year']
 
+        return year_query
+
+    def get_movie_search_query_string(self, options):
+        return urljoin(self.BASE_API_URL, 'search/movie?query=' + options['title'] + '&api_key='+ self.API_KEY)  #NOQA
+
+    def get_movie_query_string(self, options):
+        return urljoin(self.BASE_API_URL, 'movie/' + str(options['id']) + '?api_key='+ self.API_KEY)  #NOQA
+
+    def get_movie_video_query_string(self, options):
+        return urljoin(self.BASE_API_URL, 'movie/' + str(options['id']) + '/videos?api_key='+ self.API_KEY)  #NOQA
+
+    def get_movie_id(self, options):
+        # if there is a year specification, use it,
+        year_query = self.get_year_query(options)
+
         # otherwise default to first search result based on title
-        api_url = urljoin(BASE_API_URL, 'search/movie?query=' + options['title'] + '&api_key='+ API_KEY + year_query)  #NOQA
+        api_url = self.get_movie_search_query_string(options) + year_query
         connection = urllib.urlopen(api_url)
         id = json.loads(connection.read())['results'][0]['id']
         connection.close()
         return id
 
-    def get_movie_data(self, movie_id):
+    def get_movie_data(self, options):
         # get poster image and other related movie data for specified movie
-        data_url = urljoin(BASE_API_URL, 'movie/' + str(movie_id) + '?api_key=' + API_KEY)  #NOQA
+        data_url = self.get_movie_query_string(options)
         connection = urllib.urlopen(data_url)
         data = json.loads(connection.read())
         connection.close()
         return data
 
-    def get_trailer_url(self, movie_id):
+    def get_trailer_url(self, options):
         # get trailer data for specified movie
-        data_url = urljoin(BASE_API_URL, 'movie/' + str(movie_id) + '/videos?api_key=' + API_KEY)  #NOQA
+        data_url = self.get_movie_video_query_string(options)
         connection = urllib.urlopen(data_url)
         results = json.loads(connection.read())['results']
         connection.close()
--- src/media_test.py
+++ src/media_test.py
@@ -2,6 +2,11 @@ import media
 import json
 from mock import Mock, patch
 from nose.tools import *
+import urllib
+try:
+    from urllib.parse import urljoin
+except ImportError:
+    from urlparse import urljoin
 
 test_options = { 'title':'Toy Story', 'year': '1999' }
 
@@ -42,12 +47,85 @@ def test_init_sets_self_data_metadata_from_api(mock_get_movie_data):
     assert_true(test_movie.data.has_key('baz'))
 # end __init__
 
-# get_movie_id
+# get_year_query
+def test_get_year_query_uses_year_if_provided():
+    test_movie = media.Movie(test_options)
+    eq_(test_movie.get_year_query({ 'year': '2000' }), '&year=2000')
+
+def test_get_year_query_returns_empty_if_no_year():
+    test_movie = media.Movie(test_options)
+    eq_(test_movie.get_year_query({}), '')
+# end get_year_query
+
+# get_movie_search_query_string
+def test_get_movie_search_query_returns_query_with_title():
+    test_movie = media.Movie(test_options)
+    test_movie.BASE_API_URL = 'base_url'
+    test_movie.API_KEY = 'newkey'
+    movie_title = 'something'
+    movie_options = { 'title': movie_title }
+    api_query_string = urljoin(test_movie.BASE_API_URL, 'search/movie?query=' + movie_title + '&api_key=' + test_movie.API_KEY)
+
+    eq_(test_movie.get_movie_search_query_string(movie_options), api_query_string)
+# end get_movie_search_query_string
+
+# get_movie_query_string
+def test_get_movie_query_returns_query_with_id():
+    test_movie = media.Movie(test_options)
+    test_movie.BASE_API_URL = 'base_url'
+    test_movie.API_KEY = 'newkey'
+    movie_id = 12
+    movie_options = { 'id': movie_id }
+    api_query_string = urljoin(test_movie.BASE_API_URL, 'movie/' + str(movie_id) + '?api_key=' + test_movie.API_KEY)
+
+    eq_(test_movie.get_movie_query_string(movie_options), api_query_string)
+# end get_movie_query_string
+
+# get_movie_video_query_string
+def test_get_movie_video_query_returns_query_with_id():
+    test_movie = media.Movie(test_options)
+    test_movie.BASE_API_URL = 'base_url'
+    test_movie.API_KEY = 'newkey'
+    movie_id = 12
+    movie_options = { 'id': movie_id }
+    api_query_string = urljoin(test_movie.BASE_API_URL, 'movie/' + str(movie_id) + '/videos?api_key=' + test_movie.API_KEY)
+
+    eq_(test_movie.get_movie_video_query_string(movie_options), api_query_string)
+# end get_movie_video_query_string
+
+# TODO: make this test_movie context-specific only for following tests
 test_movie = media.Movie(test_options)
+# get_movie_id
 @patch('media.urllib.urlopen')
 def test_get_movie_id_returns_id(mock_urlopen):
     a = Mock()
     a.read.side_effect = [json.dumps({ 'results': [{ 'id': 2 }] })]
     mock_urlopen.return_value = a
-    idValue = test_movie.get_movie_id(test_options)
-    assert idValue == 2
+    id_value = test_movie.get_movie_id(test_options)
+    assert id_value == 2
+#end get_movie_id
+
+# get_movie_data
+@patch('media.urllib.urlopen')
+def test_get_movie_data_returns_data(mock_urlopen):
+    mock_response = { 'nothing': [{ 'really': 'matters' }] }
+    a = Mock()
+    a.read.side_effect = [json.dumps(mock_response)]
+    mock_urlopen.return_value = a
+    test_options['id'] = 5
+    response_value = test_movie.get_movie_data(test_options)
+    eq_(response_value, mock_response)
+# end get_movie_data
+
+# get_trailer_url
+@patch('media.urllib.urlopen')
+def test_get_trailer_url_returns_url(mock_urlopen):
+    mock_response = 'newurl'
+    a = Mock()
+    a.read.side_effect = [json.dumps({ 'results': [{ 'key': mock_response }]})]
+    mock_urlopen.return_value = a
+    test_options['id'] = 5
+    response_value = test_movie.get_trailer_url(test_options)
+    eq_(response_value, mock_response)
+# end get_trailer_url
+
